f74b18085c4c32ead59e8f48b9a0ea2545666772
Igor Park <i.pak@ktsstudio.ru>
2018-12-16 03:11:50 +0300

    Permissions

--- app/src/main/java/com/z/glad2see/ui/note_list/MainActivity.java
+++ app/src/main/java/com/z/glad2see/ui/note_list/MainActivity.java
@@ -1,16 +1,14 @@
 package com.z.glad2see.ui.note_list;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.z.glad2see.R;
 import com.z.glad2see.model.DataUtils;
 import com.z.glad2see.ui.contact_list.mvp.ContactListActivity;
@@ -28,47 +26,29 @@ public class MainActivity extends MvpAppCompatActivity implements MainActivityVi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_list_activity);
 
-        initFAB();
+        initFab();
 
         recyclerView = findViewById(R.id.my_recycler_view);
         NoteListAdapter adapter = new NoteListAdapter(DataUtils.generateNotes(), this);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
-
-        requestPermissions();
     }
 
-    void initFAB(){
+    void initFab() {
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> startActivity(ContactListActivity.getStartIntent(this)));
-    }
-
-    private void requestPermissions() {
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) !=
-                PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_PHONE_STATE)) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) !=
-                PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_CALL_LOG)) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_CALL_LOG}, 1);
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_CALL_LOG}, 1);
-            }
-        }
+        fab.setOnClickListener(view -> {
+            RxPermissions rxPermissions = new RxPermissions(this);
+            rxPermissions.request(Manifest.permission.READ_CONTACTS)
+                    .subscribe(isGranted ->
+                            {
+                                if (isGranted) {
+                                    startActivity(ContactListActivity.getStartIntent(this));
+                                }
+                            },
+                            Throwable::printStackTrace
+                    );
+        });
     }
 
     @Override
